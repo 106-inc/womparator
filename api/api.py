e160df98ad0507c996f9241ab0784a2a5521d363
kilka-rav <zakirianov.rv@phystech.edu>
2024-04-14 17:08:15 +0300

    first working version of APIRequest

--- api/api.py
+++ api/api.py
@@ -1,11 +1,14 @@
 import json
+import requests
+import os
 
 class APIRequest():
-    def __init__(self, text = "") -> None:
+    def __init__(self, text = "", system_role = "") -> None:
         self.text = text
-        self.folder_id = #your folder id
-        self.api = #your api
-
+        self.system_role = system_role
+        self.prompt = {}
+        self.folder_id = os.getenv("YC_FOLDER_ID", "")
+        self.api = os.getenv("YC_IAM_TOKEN", "")
     def createURLModel(self):
         model = "gpt://" + self.folder_id + "/yandexgpt-lite"
         return model
@@ -15,15 +18,16 @@ class APIRequest():
         completeOptions["stream"] = False
         completeOptions["temperatute"] = 0.6
         completeOptions["maxTokens"] = 2000
+        return completeOptions
 
     def createMessages(self):
         message1 = {}
         message2 = {}
         message1["role"] = "system"
-        message1["text"] = "Ты профессионал"
+        message1["text"] = self.system_role
         message2["role"] = "user"
         message2["text"] = self.text
-        return message1, message2
+        return [message1, message2]
 
     def createJSON(self):
         print(self.text)
@@ -31,12 +35,31 @@ class APIRequest():
 
         json_file["modelUri"] = self.createURLModel()
         json_file["completionOptions"] = self.createCompleteOptions()
-        json_file["messages"] = [self.createMessages()]
+        json_file["messages"] = self.createMessages()
         
         json_data = json.dumps(json_file, ensure_ascii=False, indent=4)
 
-        with open("request.json", "w") as outfile:
-            outfile.write(json_data)
+        self.json = json_file
+        self.prompt = json_data
+        #with open("request.json", "w") as outfile:
+        #    outfile.write(json_data)
 
-a = APIRequest("try")
-a.createJSON()
+    def send_request(self):
+
+        url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+
+        headers = {}
+        headers["Content-Type"] = "application/json"
+        headers['Authorization'] = 'Bearer ' + str(self.api)
+        headers['x-folder-id'] = self.folder_id
+
+        response = requests.post(url, headers=headers, json=self.json)
+        result = response.text
+        return result
+
+    def run(self):
+        self.createJSON()
+        self.send_request()
+
+a = APIRequest("Превед медвед", "Найди ошибки в тексте")
+a.run()
