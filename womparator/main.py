875a90caf71bedb81155085035ee37283f2b9db7
Bryanskiy <ivakin.kir@gmail.com>
2024-04-14 19:42:51 +0300

    rename directories

--- womparator/main.py
+++ womparator/main.py
@@ -2,21 +2,16 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from flask import request
 
-app = Flask(__name__)
-CORS(app)
+womparator = Flask(__name__)
+CORS(womparator)
 
 
-@app.route("/upload", methods=["POST"])
+@womparator.route("/upload", methods=["POST"])
 def upload():
     print(request.data)
     response = "Whatever you wish too return"
     return response
 
 
-@app.route("/api/hello", methods=["GET"])
-def hello():
-    return jsonify({"message": "Pixies Team Production"})
-
-
 if __name__ == "__main__":
-    app.run(port=8080)
+    womparator.run(port=8080)
