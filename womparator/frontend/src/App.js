5c19c85bd4ae1d3013b61378ea7ed608e754f544
Bryanskiy <ivakin.kir@gmail.com>
2024-04-14 21:20:21 +0300

    [front] improve stype

--- womparator/frontend/src/App.js
+++ womparator/frontend/src/App.js
@@ -2,62 +2,145 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [fileName, setFileName] = useState(null);
+  const [preview, setPreview] = useState(null);
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [buttonText, setButtonText] = useState('Select your file first');
 
-  function showFile() {
-    if (window.File && window.FileReader && window.FileList && window.Blob) {
-         var preview = document.getElementById('show-text');
-         var file = document.querySelector('input[type=file]').files[0];
-         var reader = new FileReader()
+  const Spinner = () => (
+    <svg
+      className='spinner'
+      width='65px'
+      height='65px'
+      viewBox='0 0 66 66'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <circle
+        className='path'
+        fill='none'
+        strokeWidth='6'
+        strokeLinecap='round'
+        cx='33'
+        cy='33'
+        r='30'
+      ></circle>
+    </svg>
+  );
 
-         var textFile = /text.*/;
 
-         if (file.type.match(textFile)) {
-            reader.onload = function (event) {
-               let body = event.target.result;
-               fetch('http://localhost:8080/upload', {
-                method: 'POST',
-                body,
-              }).then((response) => {
-                response.json().then((body) => {
-                  console.log(response)
-                })
-                .catch(error => {
-                  console.error('failed to post file:', error);
-                });
-              });
-            }
-         } else {
-            preview.innerHTML = "<span class='error'>It doesn't seem to be a text file!</span>";
-         }
-         reader.readAsText(file);
+  // Handling file selection from input
+  const onFileSelected = (e) => {
+    if (e.target.files[0]) {
+      setSelectedFile(e.target.files[0]);
+      setFileName(e.target.files[0].name);
+      setIsDisabled(false); // Enabling upload button
+      setButtonText("Let's upload this!");
+    }
+  };
 
-   } else {
-      alert("Your browser is too old to support HTML5 File API");
-   }
-  }
-
+  // Setting image preview
   useEffect(() => {
-    fetch('http://localhost:8080/api/hello')
-      .then(response => {
-        console.log(response);
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        setGreeting(data.message);
-      })
-      .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-      });
-  }, []);
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onloadend = () => setPreview(reader.result);
+      reader.readAsDataURL(selectedFile);
+    }
+  }, [selectedFile]);
+
+  // Setting image preview
+  useEffect(() => {
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onloadend = () => setPreview(reader.result);
+      reader.readAsDataURL(selectedFile);
+    }
+  }, [selectedFile]);
+
+  const handleFileUpload = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setIsDisabled(true);
+    setButtonText("Wait we're uploading your file...");
+
+    try {
+      if (selectedFile !== '') {
+
+        fetch('http://localhost:8080/upload', {
+          method: 'POST',
+          body: selectedFile,
+        }).then((response) => {
+          response.json().then((body) => {
+            console.log(response)
+          })
+          .catch(error => {
+            console.error('failed to post file:', error);
+          });
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <div className="App">
-    <input type="file" onChange={showFile}/>
-    <div id="show-text">Choose text File</div>
+    <div className='app'>
+      <header className='title'>
+        <h1>Text compare</h1>
+      </header>
+      <main>
+        <form onSubmit={(e) => handleFileUpload(e)}>
+        <label className='uploader'>
+            <div className='upload-space'>
+              {isLoading ? (
+                <Spinner />
+              ) : (
+                <>
+                  {isError || isSuccess ? (
+                    <i
+                      className={`icon-${isSuccess ? 'success' : 'error'}`}
+                    ></i>
+                  ) : (
+                    <>
+                      {preview ? (
+                        <div className='preview'>
+                          <img
+                            src={preview}
+                            alt='Preview of the file to be uploaded'
+                          />
+                        </div>
+                      ) : (
+                        <i className='icon-upload'></i>
+                      )}
+                      <input type='file' onChange={onFileSelected} />
+                    </>
+                  )}
+                </>
+              )}
+            </div>
+            {isError || isSuccess ? (
+              <p className={isSuccess ? 'success' : 'error'}>
+                {isSuccess ? 'Upload successful!' : 'Something went wrong ...'}
+              </p>
+            ) : (
+              <p className='filename'>
+                {fileName ? fileName : 'No file selected yet'}
+              </p>
+            )}
+          </label>
+          <button
+            type='submit'
+            className='btn'
+            disabled={isDisabled}
+            tabIndex={0}
+          >
+            {buttonText}
+          </button>
+        </form>
+      </main>
     </div>
   );
 }
