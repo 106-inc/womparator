39fef6dc5d1e7d69249031c549d1f8883a610cd5
Bryanskiy <ivakin.kir@gmail.com>
2024-04-16 13:51:59 +0300

    [front] remove unused

--- womparator/frontend/src/App.js
+++ womparator/frontend/src/App.js
@@ -23,69 +23,29 @@ function App() {
   const [preview, setPreview] = useState(null);
   const [isDisabled, setIsDisabled] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [isSuccess, setIsSuccess] = useState(false);
   const [buttonText, setButtonText] = useState('Select your files first');
 
-  const Spinner = () => (
-    <svg
-      className='spinner'
-      width='65px'
-      height='65px'
-      viewBox='0 0 66 66'
-      xmlns='http://www.w3.org/2000/svg'
-    >
-      <circle
-        className='path'
-        fill='none'
-        strokeWidth='6'
-        strokeLinecap='round'
-        cx='33'
-        cy='33'
-        r='30'
-      ></circle>
-    </svg>
-  );
-
   const Form = () => (
-    <form onSubmit={(e) => handleFileUpload(e)}>
+    <form>
       <label className='uploader'>
         <div className='upload-space'>
-          {isLoading ? (
-            <Spinner />
-          ) : (
-            <>
-              {isError || isSuccess ? (
-                <i
-                  className={`icon-${isSuccess ? 'success' : 'error'}`}
-                ></i>
-              ) : (
-                <>
-                  {preview ? (
-                    <div className='preview'>
-                      <img
-                        src={preview}
-                        alt='Preview of the file to be uploaded'
-                      />
-                    </div>
-                  ) : (
-                    <i className='icon-upload'></i>
-                  )}
-                  <input type='file' onChange={onFileSelected} />
-                </>
-              )}
-            </>
-          )}
+          <>
+            {preview ? (
+              <div className='preview'>
+                <img
+                  src={preview}
+                  alt='Preview of the file to be uploaded'
+                />
+              </div>
+            ) : (
+              <i className='icon-upload'></i>
+            )}
+            <input type='file' onChange={onFileSelected} />
+          </>
         </div>
-        {isError || isSuccess ? (
-          <p className={isSuccess ? 'success' : 'error'}>
-            {isSuccess ? 'Upload successful!' : 'Something went wrong ...'}
-          </p>
-        ) : (
-          <p className='filename'>
-            {fileName ? fileName : 'No file selected yet'}
-          </p>
-        )}
+        <p className='filename'>
+          {fileName ? fileName : 'No file selected yet'}
+        </p>
       </label>
     </form>
   );
@@ -117,32 +77,6 @@ function App() {
       reader.readAsDataURL(selectedFile);
     }
   }, [selectedFile]);
-
-  const handleFileUpload = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-    setIsDisabled(true);
-    setButtonText("Wait we're uploading your file...");
-
-    try {
-      if (selectedFile !== '') {
-
-        fetch('/upload', {
-          method: 'POST',
-          body: selectedFile,
-        }).then((response) => {
-          response.json().then((body) => {
-            console.log(response)
-          })
-            .catch(error => {
-              console.error('failed to post file:', error);
-            });
-        });
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   return (
     <div className='app'>
